251ce2820b70e788bb545f32efa63f054c7f92c6
Isaac Hardy <isaachardyDEV@gmail.com>
2015-10-12 14:05:29 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+(function () {
+
+  // var firstItem = dragon.results[0];
+
+  var templateString = $('#itemTemplate').text();
+
+  var templateFunction = _.template(templateString);
+
+  _.each(dragon.results, function (item) {
+    var itemHTML = templateFunction(item);
+    $('.results').append(itemHTML);
+
+  });
+
+}());

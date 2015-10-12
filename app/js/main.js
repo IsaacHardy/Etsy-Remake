c5b5e48dc4dd415b18c30edb478ead0b0ba95545
Isaac Hardy <isaachardyDEV@gmail.com>
2015-10-12 15:18:00 -0400

    Added AJAX function to GET data from Etsy API and display it onto our webpage

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,25 @@
 
 (function () {
 
-  // var firstItem = dragon.results[0];
+	// Var set to our API url
+	var etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=dungeons%20and%20dragons&includes=Images,Shop';
 
-  var templateString = $('#itemTemplate').text();
+	// Ajax method from jQuery to get data from API
+	$.ajax({
+		url: etsyURL,
+		dataType: 'jsonp',
+		method: 'get'
+	}).then(function (response) {
+		console.log(response);
 
-  var templateFunction = _.template(templateString);
+		// Var to select location of our HTML using template to pipe the info
+		var templateString = $('#itemTemplate').text();
+		var templateFunction = _.template(templateString);
 
-  _.each(dragon.results, function (item) {
-    var itemHTML = templateFunction(item);
-    $('.results').append(itemHTML);
-  });
+		// Function to append each piece of data to html location using the undescore .each method
+		_.each(response.results, function (item) {
+			var itemHTML = templateFunction(item);
+			$('.results').append(itemHTML);
+		});
+	});
 })();
